4e856eb8b12261e34a8add53d7382c292ca0de12
Evan Mullins <evancmullins@gmail.com>
2022-08-31 15:13:34 -0500

    readded gpu suppoert

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, send_file
-from torch import autocast
+from torch import autocast, cuda
 from diffusers import StableDiffusionPipeline, LMSDiscreteScheduler
 app = Flask(__name__)
 
@@ -14,14 +14,20 @@ pipe = StableDiffusionPipeline.from_pretrained(
     "CompVis/stable-diffusion-v1-4", 
     scheduler=lms,
     use_auth_token=True
-).to("cuda")
+)
 
-@app.route("/stable_diffusion", )
+if cuda.is_available():
+    pipe = pipe.to("cuda")
+
+@app.route("/stable_diffusion")
 def get_result():
     prompt = request.args.get("prompt")
     prompt_path = "data/" + prompt.replace(" ", "_") + ".png"
     print(prompt, prompt_path)
-    image = pipe(prompt)["sample"][0] 
+    autocast = "cuda" if cuda.is_available() else "cpu"
+    with autocast(autocast):
+        image = pipe(prompt)["sample"][0] 
+
     image.save(prompt_path)
 
     return send_file(prompt_path, mimetype='image/png')

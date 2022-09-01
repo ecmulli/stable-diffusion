d90d95b8b484baa36bd278c33b4c2d6e0ef55865
0xb79orch <0xb79orch@gmail.com>
2022-08-31 23:32:33 -0500

    update to incl access token and clear cache

--- stable-diffusion.py
+++ stable-diffusion.py
@@ -1,6 +1,9 @@
 from torch import autocast
 from diffusers import StableDiffusionPipeline, LMSDiscreteScheduler
 from IPython.display import Image
+import os
+
+access_token = "enter access token"
 
 # this will substitute the default PNDM scheduler for K-LMS  
 lms = LMSDiscreteScheduler(
@@ -12,13 +15,16 @@ lms = LMSDiscreteScheduler(
 pipe = StableDiffusionPipeline.from_pretrained(
     "CompVis/stable-diffusion-v1-4", 
     scheduler=lms,
-    use_auth_token=True
+    use_auth_token=access_token
 ).to("cuda")
 
 prompt = "a photo of an astronaut riding a horse on mars"
-file_name = "astronaut_rides_horse.png"
+file_name = os.path.join(root_dir(), "data", "astronaut_rides_horse.png")
 
 with autocast("cuda"):
     image = pipe(prompt)["sample"][0]  
+    image.save(file_name)
     
-image.save(file_name)
+torch.cuda.empty_cache()
+def root_dir():
+    return os.path.abspath(os.path.dirname(__file__))
